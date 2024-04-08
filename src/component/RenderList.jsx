723e4977eb0da05b1c63c81927c71e295c02bc7f
Abhijit <archalke.01@gmail.com>
2024-04-08 20:49:21 +0530

    add, delete functionality added

--- src/component/RenderList.jsx
+++ src/component/RenderList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 
 const cars = [
   "Tata Motors",
@@ -9,28 +10,72 @@ const cars = [
   "Kia Motors India",
   "Toyota Kirloskar Motor",
   "Honda Cars India",
-  "Renault India",
-  "Volkswagen India",
-  "Ford India",
+  // "Renault India",
+  // "Volkswagen India",
+  // "Ford India",
 ];
 
 export default function RenderList() {
-  const [list, setList] = useState(cars);
+  const [list, setList] = useState([...cars]);
+  const [input, setInput] = useState();
+
+  function handleAdd() {
+    if (input !== undefined) {
+      setList([input, ...list]);
+      console.log([input, ...list]);
+      setInput("");
+    } else {
+      alert("Enter Company name");
+    }
+  }
+  function handleDelete(index) {
+    const updateList = list.filter((_, i) => i !== index);
+    setList(updateList);
+  }
 
   return (
     <div
       style={{
         margin: "5% 10%",
+        display: "flex",
+        flexDirection: "column",
       }}
     >
       <label>
         <h1>List Items</h1>
       </label>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <input
+          placeholder="Enter Company name"
+          style={{ width: "200px" }}
+          value={input || ""}
+          onChange={(e) => setInput(e.target.value)}
+        ></input>
+        <Button
+          variant="success"
+          style={{
+            width: "100px",
+            margin: "5px",
+            font: "15px",
+            cursor: "pointer",
+          }}
+          onClick={handleAdd}
+        >
+          +
+        </Button>
+      </div>
       <ListGroup
         style={{
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
+          marginTop: "10px",
         }}
       >
         <ListGroupItem style={{ width: "200px", fontWeight: "bold" }}>
@@ -38,7 +83,11 @@ export default function RenderList() {
         </ListGroupItem>
         {list.map((item, index) => {
           return (
-            <ListGroupItem key={index} style={{ width: "200px" }}>
+            <ListGroupItem
+              key={index}
+              style={{ width: "200px", cursor: "pointer" }}
+              onClick={() => handleDelete(index)}
+            >
               {item}
             </ListGroupItem>
           );
